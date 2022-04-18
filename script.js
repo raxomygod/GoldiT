ad48e224486fddd04dc725a67f63969d5a061ccf
Raxo <etienne.sarrail95@gmail.com>
2022-04-18 17:24:45 +0200

    still trying to improve this

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ var signs = [
 var boardSigns = [];
 var foundSigns = [];
 var prevSign = "";
-var tries = 0;
-var gameIsRunning = true;
+var gameIsRunning = false;
 
 const gameContainer = document.createElement("div");
 gameContainer.id = "game-container";
@@ -17,15 +16,37 @@ board.id = "board";
 board.className = "board";
 board.classList.toggle("checked");
 
+const startBtn = document.createElement("button");
+startBtn.id = "start-button";
+startBtn.innerText = "GOLD IT";
+
 const victoryDiv = document.createElement("div");
 victoryDiv.id = "victory-div";
-victoryDiv.innerText = "YOU GOLDED IT !";
 
+gameContainer.appendChild(board);
 gameContainer.appendChild(victoryDiv);
+document.body.appendChild(startBtn);
 document.body.appendChild(gameContainer);
 
-setBoard();
-game();
+
+startBtn.addEventListener("click", () => {
+    startBtn.style.display = "none";
+    setBoard();
+    gameContainer.style.display = "flex";
+    //boxesInnerText();
+    game();
+})
+
+
+// function boxesInnerText() {
+//     for (var i  = 0; i < signs.length; i++){
+//         const showBox = document.getElementById(boardSigns[i]);
+//         showBox.innerText = boardSigns[i];
+//         setTimeout(() => {
+//             showBox.innerText = "";
+//         }, 1000);
+//     }
+// }
 
 // add score (tries made to paint board in gold)
     // add local leaderboard
@@ -47,55 +68,62 @@ function setBoard(){
         } else {
             boardSigns.push(randomSign + "1");
             box.id = randomSign + "1";
-        }
-    
-        box.innerText = "";
+        }  
+        box.innerText = box.id.slice(0,1);
         signs.splice(signs.indexOf(randomSign), 1);
         board.appendChild(box);
     }
-    gameContainer.appendChild(board);
+    setTimeout(() => {
+        document.querySelectorAll(".boxes").forEach(box => {
+            box.innerText = "";
+            gameIsRunning = true;
+        });
+    }, 2000);
+
+    
 };
 
 function game() {
+    var tries = 0;
     for (var i = 0; i < boardSigns.length; i++){
         const sign = boardSigns[i];
-        console.log(i+1 + " : " + sign)
         const box = document.getElementById(sign);
-        
+
         box.addEventListener("click", () => {
-            box.innerText = sign.slice(0,1);
-            if (sign === prevSign || box.classList.contains("checked")){
-                console.log("same box or gold box clicked ! try again.");
-            } else {             
-                if (prevSign.length === 2){
-                    const sign1 = document.getElementById(sign);
-                    const sign2 = document.getElementById(prevSign);
-                    if (sign !== prevSign && sign.slice(0,1) === prevSign.slice(0,1)){   
-                        turnGold(sign1, true);
-                        turnGold(sign2, true);
-                        foundSigns.push(sign, prevSign);                      
+            if (gameIsRunning){
+                box.innerText = sign.slice(0,1);
+                if (sign === prevSign || box.classList.contains("checked")){
+                    console.log("same box or gold box clicked ! try again.");
+                } else {             
+                    if (prevSign.length === 2){
+                        const sign1 = document.getElementById(sign);
+                        const sign2 = document.getElementById(prevSign);
+                        if (sign1.innerText === sign2.innerText){   
+                            turnGold(sign1, true);
+                            turnGold(sign2, true);
+                            foundSigns.push(sign, prevSign);                      
+                        } else {
+                            // if not matching
+                            [sign1, sign2].forEach(item => 
+                                item.style.backgroundColor = "#FF1122"); 
+                            setTimeout(() => {
+                                sign1.style.backgroundColor = "#000000";
+                                sign1.innerText = "";
+                                sign2.style.backgroundColor = "#000000";
+                                sign2.innerText = "";
+                            }, 500);
+                        }
+                        prevSign = "";
+                        tries++;
                     } else {
-                        [sign1, sign2].forEach(item => 
-                            item.style.backgroundColor = "#FF1122"); 
-                        setTimeout(() => {
-                            sign1.style.backgroundColor = "#000000";
-                            sign1.innerText = "";
-                            sign2.style.backgroundColor = "#000000";
-                            sign2.innerText = "";
-                        }, 500);
+                        prevSign = sign;
                     }
-                    prevSign = "";
-                    tries++;
-                } else {
-                    prevSign = sign;
+                    if (foundSigns.sort().toString() === boardSigns.sort().toString()){
+                        victory(tries);
+                        return;    
+                    }              
                 }
-            if (foundSigns.sort().toString() === boardSigns.sort().toString()){
-                gameIsRunning = false;   
-                //board.style.zoom = "50%";
-                return;    
             }
-                
-        }
         });
     }
 };
@@ -107,4 +135,14 @@ function turnGold(sign, statut) {
         sign.style.textShadow = "1px 2px 2px #FFFFFF";
         sign.classList.toggle("checked");
     }
-}   
+}; 
+
+function victory(tries) { 
+    board.style.zoom = "50%";
+    victoryDiv.innerText = `YOU GOLDED IT IN ${tries} TRIES!`;
+    victoryDiv.style.display = "flex";
+    document.querySelectorAll(".boxes").forEach(box => {
+        box.style.border = "1px solid #000000";
+    });
+    
+}
